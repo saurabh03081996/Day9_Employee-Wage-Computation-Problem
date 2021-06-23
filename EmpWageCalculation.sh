6e0dc75f1f5351e7775662331fa809b35e78503e
saurabhkawade784 <saurabhkawade784@gmail.com>
2021-06-23 22:45:06 +0530

    Calculating Wages For Month

--- EmpWageCalculation.sh
+++ EmpWageCalculation.sh
@@ -5,18 +5,27 @@ echo "welcome to EmpWageComputation Problem"
 isFullTime=1
 isPartTime=0
 empWagePerHr=20
-randomCheck=$((RANDOM%3))
+totalSalary=0
+totalWorkingDays=20
 
-case $randomCheck in 
-     $isFullTime)
-   	  empHrs=8
-          ;;
-     $isPartTime)
-	  empHrs=4
-	  ;;
-     *)
-	  empHrs=0
-	  ;;
-esac
-empSalary=$(( $empWagePerHr*$empHrs ))
+for (( day=1;day<=$totalWorkingDays;day++ ))
+do
+	randomCheck=$((RANDOM%3))
+
+	case $randomCheck in 
+     		$isFullTime)
+   	  		empHrs=8
+          		;;
+     		$isPartTime)
+	  		empHrs=4
+	  		;;
+     		*)
+	  		empHrs=0
+	  		;;
+	esac
+	empSalary=$(( $empWagePerHr*$empHrs ))
+	totalSalary=$(( $totalSalary+$empSalary))
+
+done
 echo "Employee Wage is: " $empSalary
+echo "Total salary of employee: "$totalSalary

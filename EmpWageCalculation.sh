cad176010699045dbf1e9bb376ef9f37516d9714
saurabhkawade784 <saurabhkawade784@gmail.com>
2021-06-23 11:14:34 +0530

    Employee Attendance

--- EmpWageCalculation.sh
+++ EmpWageCalculation.sh
@@ -1,3 +1,13 @@
-#!bin/bash -x
+#! /bin/bash -x
 
 echo "welcome to EmpWageComputation Problem"
+isPresent=1
+isAbsent=0
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
